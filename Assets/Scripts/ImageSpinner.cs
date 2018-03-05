107d109f017a9eae94f1214c00b2365c50be1f34
Jason Prindle <prindle.jason@gmail.com>
2018-03-05 00:10:45 -0500

    Added splash screen.  Improved game scene.

---
+++ Assets/Scripts/ImageSpinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSpinner : MonoBehaviour {
+
+	public float speed = 180f;	//degree rotation per second
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneChanger : MonoBehaviour {
+
+	public float countdown = 5f;
+	public Text textComponent;
+
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textComponent.text = GetRemainingTime ().ToString();
+
+		if (GetRemainingTime () <= 0) {
+			SceneManager.LoadScene (1); //Loads game scene...
+		}
+	}
+
+	private int GetRemainingTime() {
+		return Mathf.CeilToInt (countdown - GetElapsedTime());
+	}
+
+	private float GetElapsedTime() {
+		return Time.time - startTime;
+	}
+}

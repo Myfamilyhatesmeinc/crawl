492b9d0dc481534a36d31dadfd5c35a616786b7d
Ian <ian@qstartlabs.com>
2018-03-14 17:14:16 -0400

    initial commit

---
+++ Assets/Scripts/GlobalMonies.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalMonies : MonoBehaviour {
+
+	public static int MoneyCount;
+	public GameObject MoneyDisplay;
+	public int InternalMonies;
+
+	void Update () {
+		InternalMonies = MoneyCount;
+		MoneyDisplay.GetComponent<Text>().text = "Monies: " + InternalMonies;
+	}
+}
---
+++ Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour {
+
+	public GameObject textBox;
+	private int ClickMultiplier = 1;
+	public bool FirstUpgradeBought = false;
+	public bool SecondUpgradeBought = false;
+	public bool ThirdUpgradeBought = false;
+	public bool FourthUpgradeBought = false;
+	public bool FifthUpgradeBough = false;
+
+	public void ClickTheButton() {
+		GlobalMonies.MoneyCount += (1*ClickMultiplier);	
+	}
+
+	public void BuyFirstUpgrade() {
+		FirstUpgradeBought = true;
+		ClickMultiplier += 1;
+	}
+
+	public void BuySecondUpgrade() {
+		SecondUpgradeBought = true;
+		ClickMultiplier += 2;
+	}
+
+	public void BuyThirdUpgrade() {
+		ThirdUpgradeBought = true;
+		ClickMultiplier += 3;
+	}
+
+	public void BuyFourthUpgrade() {
+		FourthUpgradeBought = true;
+		ClickMultiplier += 4;
+	}
+
+	public void BuyFifthUpgrade() {
+		FifthUpgradeBough = true;
+		ClickMultiplier += 5;
+	}
+}
